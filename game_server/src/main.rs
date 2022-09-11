6ed8f81f57ed90e718c55c1a71e0f25c9e75060e
Emilio Parker <emilio@motmot.tech>
2022-09-11 13:34:30 -0600

    spawn task for each client

--- game_server/src/main.rs
+++ game_server/src/main.rs
@@ -1,87 +1,76 @@
+use socket2::Socket;
+use socket2::Type;
 use tokio::time::sleep;
 use tokio::time::Duration;
-use tokio::net::UdpSocket;
-use std::convert::TryInto;
-use std::net::IpAddr;
-use std::net::Ipv4Addr;
-use std::net::SocketAddr;
-use std::str::FromStr;
-use std::sync::Arc;
+use std::collections::HashSet;
 
 #[tokio::main]
 async fn main() {
-    let sock = UdpSocket::bind("127.0.0.1:11004").await.unwrap();
-    let mut buf = [0; 1024];
+    let mut clients:HashSet<std::net::SocketAddr> = HashSet::new();
+    let address: std::net::SocketAddr = "0.0.0.0:11004".parse().unwrap();
+    let udp_socket = create_reusable_udp_socket(address);
+
+    let mut buf_udp = [0u8; 1024];
     loop {
-        let (len, addr) = sock.recv_from(&mut buf).await.unwrap();
-        println!("{:?} bytes received from {:?}", len, addr);
-
-        if let Some(message_type) = buf.get(0)
-        {
-            println!("got a message of type at 0 {message_type}");
+        let result = udp_socket.recv_from(&mut buf_udp);
+        if let Ok((size, from_address)) = result {
+            println!("Parent: {:?} bytes received from {}", size, from_address);
+            if !clients.contains(&from_address)
+            {
+                println!("--- create child!");
+                clients.insert(from_address);
+                spawn_client_process(address, from_address);
+            }
         }
-        // if let Some(message_type) = buf.get(1)
-        // {
-        //     println!("got a message of type at 1 {message_type}");
-        // }
-
-        // let content_type_bytes = [buf[1], buf[2]];
-
-        let num = u16::from_le_bytes(buf[1..=2].try_into().unwrap());
-        println!("the message is an {num}");
-
-
-        let len = sock.send_to(&buf[..len], addr).await.unwrap();
-        println!("{:?} bytes sent", len);
-    }
+    }   
 }
 
 
-// #[tokio::main]
-// async fn main() {
-//     let num_cpus = num_cpus::get();
-
-//     println!("num cpus {num_cpus}");
-
-//     let socketAddress = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 8084);
-
-//     let main_socket = new_udp_reuseport(socketAddress);
-    // main_socket.connect(addr).await.unwrap();
-
-    // for i in 0..num_cpus {
-    //     tokio::spawn(async move {
-    //         let mut buf_udp = [0u8; MAX_PACKET_LEN];
-    //         let udp_sock = new_udp_reuseport(local_addr);
-    //         udp_sock.connect(addr).await.unwrap();
-    
-    //         loop {
-    //             if Ok(size) = udp_sock.recv(&mut buf_udp) {
-    //                 println!("{:?} bytes received" size);
-    //             }   
-    //         }   
-    //     }   
-    // }
-// }
-
-fn new_udp_reuseport(addr: SocketAddr) -> UdpSocket {
-    let udp_sock = socket2::Socket::new(
-        if addr.is_ipv4() {
-            socket2::Domain::IPV4
-        } else {
-            socket2::Domain::IPV6
-        },
-        socket2::Type::DGRAM,
-        None,
-    )
-    .unwrap();
-    udp_sock.set_reuse_port(true).unwrap();
-    // from tokio-rs/mio/blob/master/src/sys/unix/net.rs
-    udp_sock.set_cloexec(true).unwrap();
-    udp_sock.set_nonblocking(true).unwrap();
-    udp_sock.bind(&socket2::SockAddr::from(addr)).unwrap();
-    let udp_sock: std::net::UdpSocket = udp_sock.into();
-    udp_sock.try_into().unwrap()
+fn spawn_client_process(address : std::net::SocketAddr, from_address : std::net::SocketAddr)
+{
+    tokio::spawn(async move {
+        let child_socket : std::net::UdpSocket = create_reusable_udp_socket(address);
+        child_socket.connect(from_address).unwrap();
+        let mut child_buff = [0u8; 1024];
+        loop {
+            let result = child_socket.recv(&mut child_buff);
+            if let Ok(size) = result {
+                println!("Child: {:?} bytes received on child process for {}", size, from_address);
+            }
+        }
+    });
 }
+
+fn create_reusable_udp_socket(address :std::net::SocketAddr) -> std::net::UdpSocket
+{
+    let socket = Socket::new(socket2::Domain::IPV4, Type::DGRAM, None).unwrap();
+    socket.set_reuse_port(true).unwrap();
+    socket.set_reuse_address(true).unwrap();
+    socket.bind(&address.into()).unwrap();
+    let udp_socket: std::net::UdpSocket = socket.into();
+    udp_socket
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 async fn spanw_test() {
     println!("Hello, world!");
